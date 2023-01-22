cdaae83d6e91c9ce9c92e181486a175cb281cb07
Soorria Saruva <soorria.ss@gmail.com>
2023-01-22 17:09:07 +1100

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,43 @@
+// @refresh reload
+import { ComponentProps, ParentComponent } from 'solid-js'
+
+const Wrapper: ParentComponent<ComponentProps<'div'>> = props => {
+  return (
+    <div
+      /**
+       * Hydration warning occurs when props are spread, and
+       * there's 1+ "non-spread" props and children is used
+       */
+      {...props}
+      class={props.class}
+    >
+      {() => {
+        console.trace('read props.children')
+        return props.children
+      }}
+    </div>
+  )
+}
+
+const WrappedDiv: ParentComponent = props => {
+  return (
+    <Wrapper>
+      <div style="font-weight: 900; font-size: 3rem; text-align: center">
+        {props.children}
+      </div>
+
+      {/* no warning with only function child */}
+      {/* {() => {
+        return (
+          <div style="font-weight: 900; font-size: 3rem; text-align: center">
+            {props.children}
+          </div>
+        )
+      }} */}
+    </Wrapper>
+  )
+}
+
+export default function Home() {
+  return <WrappedDiv>TEXT</WrappedDiv>
+}
